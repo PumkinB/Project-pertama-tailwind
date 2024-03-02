ee183c3b99e60774c00645bf953e7f0f342bc1ec
PumkinB <olipumknb@gmail.com>
2024-03-02 13:20:14 +0700

    updating profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./profile.html"],
   theme: {
     container: {
       center: true,
